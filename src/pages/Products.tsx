84d623d5db2b6ace2733f9bcaacbe018af9ed8cc
Sreehari Aranghat <sree@aranghattech.com>
2024-05-16 12:30:18 +0530

    feat: Add category filter functionality to Products page
    The Products page has been updated to include a category filter functionality. Users can now select a category from the available options and view only the products belonging to that category. This enhancement improves the user experience by allowing users to easily browse products based on their preferred category.

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,12 +1,25 @@
+import React, { useEffect, useState } from "react";
 import ProductTile from "../components/ProductTile";
 import useProducts from "../hooks/useProducts"
 
 export default function Products() {
 
     let products = useProducts();
+    let [category, setCategory] = React.useState<string[]>([]);
+    let [selectedCategory, setSelectedCategory] = React.useState<string | null>(null);
+
+    useEffect(() => {
+        if(category.length === 0) {
+            let categories = products.map(product => product.category);
+            categories = Array.from(new Set(categories));
+            setCategory(categories);
+        }
+    }, [products]);
+
 
     return (<>
         <h1>Products</h1>
-        {products.map(product => <ProductTile key={product.id} product={product} />)}
+        {category.map(cat => <button key={cat} className="btn btn-primary m-2" onClick={()=> setSelectedCategory(cat)}>{cat}</button>)}
+        {products.filter(d =>  selectedCategory ? d.category == selectedCategory : d).map(product => <ProductTile key={product.id} product={product} />)}
     </>)
 }
