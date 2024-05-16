abe23d08bc3289ad04eb6efed7bf373d25476473
Sreehari Aranghat <sree@aranghattech.com>
2024-05-16 11:48:22 +0530

    feat: Update ProductDetails page to display product details
    The ProductDetails page has been modified to display the product details, including the product image, title, description, price, discount, and buttons for adding to cart and buying now. This enhancement improves the user experience by providing comprehensive information about the product on the details page.

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -8,7 +8,23 @@ export default function ProductDetails() {
     
     return(<>
         {product ? <>
-            <h1>{product?.title}</h1>
+
+            <div className="row">
+                <div className="col">
+                    <img src={product.thumbnail} alt={product.title} className="img-fulid" />
+                </div>
+                <div className="col">
+                    <h1>{product.title}</h1>
+                    <p>{product.description}</p>
+                    <h2>Rs. {product.price}</h2>
+                    <p>Discount: {product.discountPercentage}%</p>
+                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-secondary ms-4">Buy Now</button>
+                </div>
+            </div>
+
+            <h4 className="my-4">Similar Products</h4>
+
         </> : "Sorry product not found"}
     </>)
 }
