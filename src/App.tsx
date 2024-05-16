476ece1410e29709fa418911086a884022f4264b
Sreehari Aranghat <sree@aranghattech.com>
2024-05-16 10:21:14 +0530

    chore: Update routes file and add new route for "/" path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='container-fluid bg-light'>
-         <h1>PRoducts</h1>
+    <div className='container-fluid bg-light p-0 m-0 vh-100'>
+        <nav className="nav bg-dark text-white py-4">
+         
+           <div className="container-fluid">  <div className="row">
+            <div className="col">
+                <h1>My Cart</h1>
+            </div>
+            <div className="col-auto">
+                <a href='/register'>Register</a>
+            </div>
+          </div>
+           </div>
+        </nav>
+        <div className="container-fluid">
+          <div className="row">
+             <div className="col">
+               <Outlet />
+             </div>
+          </div>
+        </div>
     </div>
   );
 }
--- src/common/routes.ts
+++ src/common/routes.ts
@@ -1,9 +1,13 @@
 import path from "path";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import { Children, Component } from "react";
+import Products from "../pages/Products";
 
 const routes = [
-    { path : "/", Component : App }
+    { path : "/", Component : App, children : [
+        {path : "/", Component : Products}
+    ]}
 ];
 
 const router = createBrowserRouter(routes);
