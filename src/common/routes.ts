5e5d824749f5a8568928afa2fcba4c3ee785cf95
Sreehari Aranghat <sree@aranghattech.com>
2024-05-16 10:03:00 +0530

    chore: Add routes file and update dependencies

---
+++ src/common/routes.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const routes = [
+    { path : "/", Component : App}
+]
+
+export default routes;
